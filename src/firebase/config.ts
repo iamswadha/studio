5148af298d731e207ae4cd8f9b62b9893c73e10d
SWADHA SHARMA <creativeswadha@gmail.com>
2025-10-03 10:29:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5277752705-83328",
+  "appId": "1:736630937115:web:f10a5effae8c4bbd254abe",
+  "apiKey": "AIzaSyA3n4d_l6yxnj07H5yfuJFAHNAg2RfSsEE",
+  "authDomain": "studio-5277752705-83328.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "736630937115"
+};
